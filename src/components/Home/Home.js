5616d54f6f9d186f6479433d9c2d914c4608c7e2
shaun210 <Shaun.soobagrah@mail.mcgill.ca>
2024-02-15 20:55:02 -0500

    added Parking reservation software information

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,72 +7,66 @@ import linkedinIcon from "./boostrapIcon/linkedin.svg";
 import mailIcon from "./boostrapIcon/envelope-fill.svg";
 import codingPerson from "../../Assets/codingPerson.json";
 import DisplayLottie from "../Utils/DisplayLottie";
+
+
 function Home() {
-    return (
-        <section>
-            <Container fluid className="home-content " style={{ marginTop:'5rem', height:'95vh'}}>
-                <Row xs={12} style={{width:'100%'}}>
-                    <Col md={8} className="home-header">
-         
-                            <p style={{marginBottom:'3rem'}} className="heading">
-                                <strong>
-                                Hi there!{" "}
-                                <span className="wave" role="img" aria-labelledby="wave">
-                                👋🏻
-                                </span>{" "}
-                                I'm Shaun Soobagrah
-                                </strong>
-                            </p>
-      
-                            <div>
-                                <p className="greating-text">
-                                    I am a Software Developer with a passion for creating
-                                    innovative solutions to complex problems. I am a
-                                    self-motivated individual who is always eager to learn and
-                                    improve my skills. I am a team player who is able to work
-                                    effectively with others to achieve common goals. I am
-                                    currently seeking a challenging and rewarding role in the
-                                    IT industry.{" "}
-                                </p>
-                            </div>
+  return (
+    <section>
+      <Container fluid className="home-content" style={{ marginTop: '5rem', height: '95vh' }}>
+        <Row className="justify-content-center align-items-center" style={{ width: '100%', height: '100%' }}>
+          <Col xs={12} s={8} md={8} className="home-header">
+            <p style={{ marginBottom: '3rem' }} className="heading">
+              <strong>
+                Hi there!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>{" "}
+                I'm Shaun Soobagrah
+              </strong>
+            </p>
 
-                            <div className="d-flex align-items-center">
-                                <Button className="greeting-button" >
-                                    <Image src={githubIcon} className="image-button" />
-                                </Button>
-                                <Button className="greeting-button" >
-                                    <Image src={linkedinIcon} className="image-button" />
-                                </Button>
-                                <Button className="greeting-button" >
-                                    <Image src={mailIcon} className="image-button" />
-                                </Button>
-                                <Button className="greeting-button" >
-                                    <strong>Resume</strong>
-                                </Button>
-                                <DisplayLottie animationData={codingPerson} width={300} height={300} />
-                        </div>
-                   
-                    </Col>
+            <div>
+              <p className="greating-text">
+                I am a Software Developer with a passion for creating
+                innovative solutions to complex problems. I am a
+                self-motivated individual who is always eager to learn and
+                improve my skills. I am a team player who is able to work
+                effectively with others to achieve common goals. I am
+                currently seeking a challenging and rewarding role in the
+                IT industry.{" "}
+              </p>
+            </div>
 
-                    <Col style={{ marginLeft:'200px' }}>
-                        <img
-                            src={homePic}
-                            alt="home pic"
-                            className="img-fluid rounded-circle"
-                            style={{ paddingTop: 50, height:'auto'}}
-                        />
-                    </Col>
-                </Row>
+            <div className="d-flex align-items-center">
+                <Button className="greeting-button" onClick={() => window.open('https://github.com/shaun210', '_blank')}>
+                    <Image src={githubIcon} className="image-button" />
+                </Button>
+                <Button className="greeting-button" onClick={() => window.open('https://www.linkedin.com/in/shaun-soobagrah/', '_blank')}>
+                    <Image src={linkedinIcon} className="image-button" />
+                </Button>
+                <Button className="greeting-button" onClick={() => window.open('mailto:ssoobagrah@gmail.com')}>
+                    <Image src={mailIcon} className="image-button" />
+                </Button>
+                <Button className="greeting-button" onClick={() => window.open('./resume.pdf', '_blank')}>
+                    <strong>Resume</strong>
+                </Button>
 
+              <DisplayLottie animationData={codingPerson} width={300} height={300} />
+            </div>
+          </Col>
 
-                {/* <Row className="justify-content-center" style={{ paddingBottom: 20, marginLeft:'40vw' }}>
-                    <Col xs={12} md={12}>
-                    <DisplayLottie animationData={codingPerson} width={500} height={500} />
-                    </Col>
-                </Row> */}
-            </Container>
-        </section>
-    );
+          <Col xs={12} s={4} md={4} className="d-flex justify-content-center">
+            <img
+              src={homePic}
+              alt="home pic"
+              className="img-fluid rounded-circle"
+              style={{ paddingTop: 50, height: 'auto' }}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
 export default Home;
--- src/components/Skill/skills.js
+++ src/components/Skill/skills.js
@@ -1,65 +1,81 @@
 import React from 'react';
 import { Container, Row, Col, Image, ProgressBar } from 'react-bootstrap';
 import universityLogo from './McGill University.svg';
+import GitHubCalendar from 'react-github-calendar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faJava, faJs, faHtml5, faCss3, faPython, faPhp, faNodeJs, faDocker, faAws, faGit, faJira } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+
 
 const SkillsPage = () => {
   const skills = [
-    { name: 'Java', category: 'Backend', rating: 8 },
-    { name: 'React', category: 'Frontend', rating: 7 },
-    { name: 'JavaScript', category: 'Frontend', rating: 8 },
-    { name: 'HTML', category: 'Frontend', rating: 9 },
-    { name: 'CSS', category: 'Frontend', rating: 8 },
-    { name: 'Node.js', category: 'Backend', rating: 7 },
-    { name: 'Python', category: 'Backend', rating: 6 },
-    { name: 'Docker', category: 'Other Tools', rating: 7 },
-    { name: 'AWS', category: 'Other Tools', rating: 6 },
+    { name: 'Java (Spring Boot)', icon: faJava, category: 'Backend' },
+    { name: 'React JS', icon: faJs, category: 'Frontend' },
+    { name: 'Vue JS', icon: faJs, category: 'Frontend' },
+    { name: 'JavaScript', icon: faJs, category: 'Frontend' },
+    { name: 'HTML', icon: faHtml5, category: 'Frontend' },
+    { name: 'CSS', icon: faCss3, category: 'Frontend' },
+    { name: 'Node.js', icon: faNodeJs, category: 'Backend' },
+    { name: 'Python (Flask)', icon: faPython, category: 'Backend' },
+    { name: 'PHP', icon: faPhp, category: 'Backend' },
+    { name: 'PostgreSQL', icon: faDatabase, category: 'Backend' },
+    { name: 'MySql', icon: faDatabase, category: 'Backend' },
+    { name: 'Docker', icon: faDocker, category: 'Other Tools' },
+    { name: 'AWS', icon: faAws, category: 'Other Tools' },
+    { name: 'Git', icon: faGit, category: 'Other Tools' },
+    { name: 'Jira', icon: faJira, category: 'Other Tools' },
     // Add more skills as needed
   ];
 
-  const renderProgressBar = (rating) => {
-    return <ProgressBar now={rating * 10} label={`${rating}/10`} />;
+
+  const SkillSection = ({ title, skills }) => {
+    const renderSkillRows = () => {
+      const rows = [];
+      const itemsPerRow = 4; // Adjust as needed based on the design and screen size
+  
+      for (let i = 0; i < skills.length; i += itemsPerRow) {
+        const rowSkills = skills.slice(i, i + itemsPerRow);
+  
+        const rowItems = rowSkills.map((skill, index) => (
+          <Col key={index} xs={12} sm={6} md={3}>
+            <div className="skill-item">
+              <FontAwesomeIcon icon={skill.icon} style={{ marginRight: '0.5rem', color: '#007bff', fontSize: '2rem' }} />
+              <p>{skill.name}</p>
+            </div>
+          </Col>
+        ));
+  
+        rows.push(
+          <Row key={i} className="mt-3">
+            {rowItems}
+          </Row>
+        );
+      }
+  
+      return rows;
+    };
+  
+    return (
+      <div className="skill-section mt-4" style={{ backgroundColor: 'rgb(231, 223, 234)', borderRadius: '20px', padding: '15px' }}>
+        <h3>{title}</h3>
+        {renderSkillRows()}
+      </div>
+    );
   };
+  
+  
 
   return (
     <Container style={{ padding: '5rem', textAlign: 'left' }}>
       <Row className="mt-5">
-        <Col>
-          <h1>Programming Skills</h1>
-        </Col>
-      </Row>
-      <Row className="mt-4">
-        <Col>
-          <h3>Frontend</h3>
-          {skills.filter(skill => skill.category === 'Frontend').map((skill, index) => (
-            <div key={index}>
-              <p>{skill.name}</p>
-              {renderProgressBar(skill.rating)}
-            </div>
-          ))}
-        </Col>
-      </Row>
-      <Row className="mt-4">
-        <Col>
-          <h3>Backend</h3>
-          {skills.filter(skill => skill.category === 'Backend').map((skill, index) => (
-            <div key={index}>
-              <p>{skill.name}</p>
-              {renderProgressBar(skill.rating)}
-            </div>
-          ))}
-        </Col>
-      </Row>
-      <Row className="mt-4">
-        <Col>
-          <h3>Other Tools</h3>
-          {skills.filter(skill => skill.category === 'Other Tools').map((skill, index) => (
-            <div key={index}>
-              <p>{skill.name}</p>
-              {renderProgressBar(skill.rating)}
-            </div>
-          ))}
-        </Col>
-      </Row>
+    <Col>
+      <h1>Programming Skills</h1>
+    </Col>
+  </Row>
+  <SkillSection title="Frontend" skills={skills.filter(skill => skill.category === 'Frontend')} />
+  <SkillSection title="Backend" skills={skills.filter(skill => skill.category === 'Backend')} />
+  <SkillSection title="Other Tools" skills={skills.filter(skill => skill.category === 'Other Tools')} />
+
       {/* Education Section */}
       <Row className="mt-5">
         <Col>
@@ -74,6 +90,13 @@ const SkillsPage = () => {
           <p><strong>CGPA:</strong> 3.8/4.0</p>
           <p><strong>Duration:</strong> August 2019 - May 2023</p>
           <p><strong>Achievements:</strong> Dean's List, Academic Excellence Award</p>
+        </Col>
+      </Row>
+
+      <Row className="w-100 mt-4">
+        <Col>
+          <h1 style={{ marginBottom: '2rem' }}>GitHub Contribution Calendar</h1>
+          <GitHubCalendar username="shaun210" colorScheme='light' style={{ width: '100%' }} />
         </Col>
       </Row>
     </Container>
